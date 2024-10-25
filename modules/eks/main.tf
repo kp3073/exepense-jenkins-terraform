34c54213eeb89b8c31677ff475659b4db2c5edbf
kp3073 <keyurpatel8288@gmail.com>
2024-10-25 13:13:09 -0400

    new11

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -34,3 +34,13 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.main.name
 }
+
+resource "null_resource" "aws-auth" {
+  depends_on = [aws_eks_cluster.main]
+  provisioner "local-exec" {
+    command = <<EOF
+aws eks update-kubeconfig --name ${var.env}-${var.project_name}
+aws-auth upsert --mapusers --userarn arn:aws:iam::471112727668:user/keyur --username keyu --groups system:masters
+  EOF
+  }
+}
