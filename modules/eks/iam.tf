b677dcb45256dbc97de133a83f842ce5c0420841
kp3073 <keyurpatel8288@gmail.com>
2024-10-31 11:48:38 -0400

    new111

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -57,3 +57,13 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryRea
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node.name
 }
+
+resource "aws_iam_openid_connect_provider" "default" {
+  url = "https://accounts.google.com"
+
+  client_id_list = [
+    "266362248691-342342xasdasdasda-apps.googleusercontent.com",
+  ]
+
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+}
